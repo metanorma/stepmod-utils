648cc0448593c0af4e8b5ebee5e57bbb171657bd
Тараскин Михаил <MITaraskin@domclick.ru>
2020-07-03 14:55:54 +0300

    #23 when there is term definition divided by semicolumn take its text as term ref, returned old README

--- lib/stepmod/utils/converters/module_ref.rb
+++ lib/stepmod/utils/converters/module_ref.rb
@@ -5,7 +5,26 @@ module Stepmod
     module Converters
       class ModuleRef < ReverseAsciidoctor::Converters::Base
         def convert(node, _state = {})
-          " term:[#{node['linkend']}] "
+          ref = node['linkend']
+          # #23:
+          # In this case when we see this:
+
+          # <module_ref linkend="product_as_individual:3_definition">individual products</module_ref>
+          # We take the text value of the element and convert to this:
+
+          # term:[individual products]
+          if node['linkend'].split(':').length > 1
+            ref = node.text
+          end
+          " term:[#{normalized_ref(ref)}] "
+        end
+
+        private
+
+        def normalized_ref(ref)
+          return unless ref || ref.empty?
+
+          ref.squeeze(' ').strip
         end
       end
       ReverseAsciidoctor::Converters.register :module_ref, ModuleRef.new
