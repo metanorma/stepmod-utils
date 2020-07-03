8c30ceb81aedf0ec67c6b06225f19f46a62a3c2c
Тараскин Михаил <MITaraskin@domclick.ru>
2020-07-03 14:43:50 +0300

    #17 Semicolon in <term> means alternative term

--- lib/stepmod/utils/converters/term.rb
+++ lib/stepmod/utils/converters/term.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
+require 'stepmod/utils/converters/synonym'
+
 module Stepmod
   module Utils
     module Converters
       class Term < ReverseAsciidoctor::Converters::Base
         def convert(node, state = {})
-          "=== #{treat_children(node, state).strip}"
+          first_child = node.children.find do |child|
+                          child.name == 'text' && !child.text.to_s.strip.empty?
+                        end
+          unless first_child &&
+                  node.text.split(';').length == 2 &&
+                  defined?(Stepmod::Utils::Converters::Synonym)
+            return "=== #{treat_children(node, state).strip}"
+          end
+
+          term_def, alt = node.text.split(';')
+          alt_xml = Nokogiri::XML::Text.new(alt, Nokogiri::XML::Document.new)
+          converted_alt = Stepmod::Utils::Converters::Synonym.new.convert(alt_xml)
+          "=== #{term_def}\n\n#{converted_alt}"
         end
       end
 
---
+++ spec/stepmod/converters/term_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Stepmod::Utils::Converters::Term do
+  let(:converter) { described_class.new }
+  let(:xml_input) do
+    '<term id="ap233_function">function; transformation</term>'
+  end
+  let(:output) do
+    "=== function\n\nalt:[transformation]"
+  end
+
+  it 'converts seimicolon text as al' do
+    input = node_for(xml_input)
+    expect(converter.convert(input).strip).to eq(output.strip)
+  end
+end
