d42232ee550e8e956e2e2fba4a54cc221cd302cd
Тараскин Михаил <MITaraskin@domclick.ru>
2020-07-03 14:55:54 +0300

    #23 when there is term definition divided by semicolumn take its text as term ref, returned old README

---
+++ spec/stepmod/converters/module_ref_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Stepmod::Utils::Converters::ModuleRef do
+  let(:converter) { described_class.new }
+  let(:xml_input) do
+    '<module_ref linkend="product_as_individual:3_definition">individual products</module_ref>'
+  end
+  let(:output) do
+    "term:[individual products]"
+  end
+
+  it 'when there is semicolum in linked attribute it converts by rules' do
+    input = node_for(xml_input)
+    expect(converter.convert(input).strip).to eq(output.strip)
+  end
+end
