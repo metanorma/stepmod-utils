b71dba3055083695c9494d6712cdc51773aee313
Тараскин Михаил <MITaraskin@domclick.ru>
2021-01-29 16:26:59 +0300

    #37 Do not escape underscores during stepmod conversion

--- spec/stepmod/converters/definition_spec.rb
+++ spec/stepmod/converters/definition_spec.rb
@@ -48,7 +48,7 @@ RSpec.describe Stepmod::Utils::Converters::Definition do
       TEXT
     end
     let(:output) do
-      "=== class of activity\n\nclass that has only\n// <clause_ref linkend=\"3_definition:individual_activity\">individual activities</clause_ref>\nterm:[individual activity] as members\n\n[example]\n====\n'Distilling' is a class of activity that is reference data held in a reference data library. The classification of the individual activity 'Distill batch\\_27 on 2006-05-19' as 'distilling' specifies what it is.\n===="
+      "=== class of activity\n\nclass that has only\n// <clause_ref linkend=\"3_definition:individual_activity\">individual activities</clause_ref>\nterm:[individual activity] as members\n\n[example]\n====\n'Distilling' is a class of activity that is reference data held in a reference data library. The classification of the individual activity 'Distill batch_27 on 2006-05-19' as 'distilling' specifies what it is.\n===="
     end
 
     it 'converts para tag into alt block' do
