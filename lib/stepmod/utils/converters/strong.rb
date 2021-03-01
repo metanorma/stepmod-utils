7a971c6366c6e51455983a7ff8fe9ffbeb6d9670
Тараскин Михаил <MITaraskin@domclick.ru>
2021-03-01 15:09:28 +0300

    #62 handle bold tag with  braces combination

--- lib/stepmod/utils/converters/strong.rb
+++ lib/stepmod/utils/converters/strong.rb
@@ -4,13 +4,33 @@ module Stepmod
   module Utils
     module Converters
       class Strong < ReverseAdoc::Converters::Base
+        BLANK_CHARS = "{blank}".freeze
+
         def convert(node, state = {})
           content = treat_children(node, state.merge(already_strong: true))
           if content.strip.empty? || state[:already_strong]
             content
           else
-            "#{content[/^\s*/]}*#{content.strip}*#{content[/\s*$/]}"
+            handle_express_escape_seq(node, "#{content[/^\s*/]}*#{content.strip}*#{content[/\s*$/]}")
           end
+        end
+
+        private
+
+        def handle_express_escape_seq(node, content)
+          res = content
+          if braces_sibling?(node.previous, true)
+            res = "#{BLANK_CHARS}#{res}"
+          end
+          if braces_sibling?(node.next)
+            res = "#{res}#{BLANK_CHARS}"
+          end
+          res
+        end
+
+        def braces_sibling?(sibling, end_of_text = false)
+          match = end_of_text ? /\($/ : /^\)/
+          sibling && sibling.text? && sibling.text =~ match
         end
       end
 
