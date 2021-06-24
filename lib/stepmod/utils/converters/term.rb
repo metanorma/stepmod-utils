208da7fea86e725c4cf6c0dae7167e7d8c4795c5
MikhailTretyakov <8926238@gmail.com>
2021-06-24 18:27:03 +0300

    https://github.com/metanorma/stepmod-utils/issues/10 Content in <term> contained within parentheses should be labeled as an acronym

--- lib/stepmod/utils/converters/term.rb
+++ lib/stepmod/utils/converters/term.rb
@@ -13,13 +13,22 @@ module Stepmod
           unless first_child &&
               node.text.split(";").length == 2 &&
               defined?(Stepmod::Utils::Converters::Synonym)
-            return "=== #{treat_children(node, state).strip}"
+            return "=== #{treat_acronym(treat_children(node, state).strip)}"
           end
 
           term_def, alt = node.text.split(";")
           alt_xml = Nokogiri::XML::Text.new(alt, Nokogiri::XML::Document.new)
           converted_alt = Stepmod::Utils::Converters::Synonym.new.convert(alt_xml)
-          "=== #{term_def}\n\n#{converted_alt}"
+          "=== #{treat_acronym(term_def)}\n\n#{converted_alt}"
+        end
+
+        private
+
+        def treat_acronym(term_def)
+          return term_def if term_def !~ /.+\(.+?\)$/
+
+          _, term_text, term_acronym = term_def.match(/(.+?)(\(.+\))$/).to_a
+          "#{term_text}\nalt:[#{term_acronym.gsub(/\(|\)/, '')}]"
         end
       end
 
