35c0e1ffa90e21fe6809f2387b4d94558df5506f
HassanAkbar <hassanakbar2@gmail.com>
2021-11-17 17:21:28 +0500

    use glossarist model in term refs #113

--- lib/stepmod/utils/converters/term.rb
+++ lib/stepmod/utils/converters/term.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "stepmod/utils/converters/synonym"
+require "glossarist"
 
 module Stepmod
   module Utils
@@ -22,22 +23,34 @@ module Stepmod
           unless first_child &&
               node.text.split(";").length == 2 &&
               defined?(Stepmod::Utils::Converters::Synonym)
-            return "=== #{treat_acronym(treat_children(node, state).strip)}"
+
+            return to_mn_adoc(
+              Glossarist::Term.from_h(
+                "definition" => treat_children(node, state).strip,
+              ),
+            )
           end
 
           term_def, alt = node.text.split(";")
           alt_xml = Nokogiri::XML::Text.new(alt, Nokogiri::XML::Document.new)
           converted_alt = Stepmod::Utils::Converters::Synonym.new.convert(alt_xml)
-          "=== #{treat_acronym(term_def)}\n\n#{converted_alt}"
+
+          term = Glossarist::Term.from_h(
+            "definition" => term_def,
+            "synonyms" => converted_alt,
+          )
+
+          to_mn_adoc(term)
         end
 
         private
 
-        def treat_acronym(term_def)
-          return term_def.strip if term_def !~ /.+\(.+?\)$/
+        def to_mn_adoc(term)
+          mn_adoc = ["=== #{term.definition}"]
+          mn_adoc << "\nalt:[#{term.acronym}]" if term.acronym
+          mn_adoc << "\n\n#{term.synonyms}" if term.synonyms
 
-          _, term_text, term_acronym = term_def.match(/(.+?)(\(.+\))$/).to_a
-          "#{term_text.strip}\nalt:[#{term_acronym.gsub(/\(|\)/, '')}]"
+          mn_adoc.join
         end
       end
 
