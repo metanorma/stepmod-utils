f13af32db1244184fb514f226935c8bb22ea3d54
Ronald Tse <ronald.tse@ribose.com>
2022-09-15 17:13:19 +0800

    feat: fully deal with `<module_ref>`, fixes #130

--- lib/stepmod/utils/converters/module_ref.rb
+++ lib/stepmod/utils/converters/module_ref.rb
@@ -5,31 +5,69 @@ module Stepmod
     module Converters
       class ModuleRef < ReverseAdoc::Converters::Base
         def convert(node, _state = {})
-          ref = node["linkend"]
-          # #23:
-          # In this case when we see this:
+          link_end = node["linkend"].to_s.split(":")
+          ref_id = link_end.last
+          parts = link_end.last.split(".")
+          text = node.text.gsub(/\s/, " ").squeeze(" ").strip
+          schema = link_end.first
 
-          # <module_ref linkend="product_as_individual:3_definition">individual products</module_ref>
-          # We take the text value of the element and convert to this:
-
-          # {{individual products}}
-
-          ref = node.text.strip
-          if !ref.empty?
-            " {{#{normalized_ref(ref)}}} "
-          elsif
-            ref = node["linkend"].split(":").first
-            " *#{ref}* "
+          if _state[:schema_and_entity].nil?
+            # puts "[module_ref] setting node state #{link_end.inspect}"
+            _state[:schema_and_entity] = schema
           end
+
+          result = case link_end[1]
+          when "1_scope", "introduction"
+            # When we see this:
+            # <module_ref linkend="functional_usage_view:1_scope">Functional usage view</module_ref>
+            # <module_ref linkend="part_definition_relationship:introduction"> Part definition relationship</module_ref>
+            # We convert into:
+            # <<express:functional_usage_view>>
+            # <<express:part_definition_relationship>>
+
+            "<<express:#{schema}>>"
+
+          when "3_definition"
+            # #23:
+            # When we see this:
+            # <module_ref linkend="product_as_individual:3_definition">individual products</module_ref>
+            # We convert to this:
+            # {{individual products}}
+
+            "{{#{text}}}"
+
+          when "4_types"
+            # When we see this:
+            # <module_ref linkend="activity_method_assignment:4_types">activity_method_item</module_ref>
+            # We convert to this:
+            # <<express:activity_method_assignment.activity_method_item>>
+            "<<express:#{[schema, text].join('.')},#{text}>>"
+          when "4_entities", "f_usage_guide"
+            case link_end[2]
+            when "figure"
+              # When we see this:
+              # <module_ref linkend="assembly_module_design:4_entities:figure:pudv">Figure 1</module_ref>
+              # We convert to this:
+              # <<figure-pudv,Figure 1>>
+              "<<#{Figure.pattern(_state,ref_id)},#{text}>>"
+            when "table"
+              # When we see this:
+              # <module_ref linkend="independent_property_definition:4_entities:table:T1">Table 1</module_ref>
+              # We convert to this:
+              # <<table-T1,Table 1>>
+              "<<#{Table.pattern(_state,ref_id)},#{text}>>"
+            end
+          else
+            puts "[module_ref]: encountered unknown <module_ref> tag, #{link_end.join(":")}"
+            raise StandardError.new("[module_ref]: encountered unknown <module_ref> tag, #{link_end.join(":")}")
+          end
+
+          # puts "[module_ref] #{result}"
+          result
         end
 
         private
 
-        def normalized_ref(ref)
-          return unless ref || ref.empty?
-
-          ref.squeeze(" ").strip
-        end
       end
       ReverseAdoc::Converters.register :module_ref, ModuleRef.new
     end
