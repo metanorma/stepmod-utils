a526313ff4f7671327b427783f303ddda19e2384
HassanAkbar <hassanakbar2@gmail.com>
2022-12-14 14:29:36 +0500

    Added code and specs for mapping changes

--- lib/stepmod/utils/change_collection.rb
+++ lib/stepmod/utils/change_collection.rb
@@ -8,19 +8,32 @@ module Stepmod
         @changes = {}
       end
 
-      def fetch_or_initialize(change, type:, resource: false)
+      def fetch_or_initialize(change, type)
         schema = schema_name(change)
 
         @changes[schema_identifier(schema, type)] ||= Change.new(
           type: type,
           stepmod_dir: @stepmod_dir,
           schema_name: schema,
-          resource: resource,
         )
+      end
+
+      def fetch(change, type)
+        schema = schema_name(change)
+        @changes[schema_identifier(schema, type)]
       end
 
       def save_to_files
         @changes.values.each(&:save_to_file)
+      end
+
+      def count
+        @changes.keys.count
+      end
+      alias_method :size, :count
+
+      def each(&block)
+        @changes.values.each(&block)
       end
 
       private
--- lib/stepmod/utils/changes_extractor.rb
+++ lib/stepmod/utils/changes_extractor.rb
@@ -29,7 +29,7 @@ module Stepmod
 
         all_modules_changes_files.each do |module_change_file|
           xml_changes = Nokogiri::XML(File.read(module_change_file)).root
-          schema_name = module_change_file.split("/")[-2]
+          schema_name = Pathname.new(module_change_file).parent.basename.to_s
           add_module_changes_to_collection(xml_changes, @collection, schema_name)
         end
 
@@ -49,11 +49,10 @@ module Stepmod
               ),
             }
 
-            add_change(
+            add_resource_changes(
               collection,
               change_edition_node,
               options,
-              type: "schema",
             )
           end
         end
@@ -65,42 +64,79 @@ module Stepmod
             options = {
               schema_name: schema_name,
               version: change_node.attr("version"),
-              description: Stepmod::Utils::SmrlDescriptionConverter.convert(
-                change_node.at("description"),
-              ),
+              description: converted_description(change_node.at("description")),
             }
 
             MODULE_TYPES.each do |type|
-              add_change(
+              add_module_changes(
                 collection,
                 change_node,
                 options,
-                type: type,
+                type,
               )
             end
           end
         end
       end
 
-      def add_change(collection, change_node, options, type:)
-        change_node.xpath("#{type}.changes").each do |changes|
-          schema_name = options[:schema_name] || changes.attr("schema_name")
-          schema_name = correct_schema_name(schema_name)
-          change = collection.fetch_or_initialize(schema_name, type: type)
+      def add_resource_changes(collection, change_node, options)
+        change_node.xpath("schema.changes").each do |changes|
+          schema_name = correct_schema_name(changes.attr("schema_name"))
+          change = collection.fetch_or_initialize(schema_name, "schema")
 
           change_edition = extract_change_edition(changes, options)
           change.add_change_edition(change_edition)
         end
       end
 
+      def add_module_changes(collection, change_node, options, type)
+        options[:type] = type
+        # assuming there will only be one change related to a type in the
+        # same version.
+        changes = change_node.xpath("#{type}.changes").first
+        schema_name = options[:schema_name]
+        change = collection.fetch_or_initialize(schema_name, type)
+
+        change_edition = extract_change_edition(changes, options)
+        change.add_change_edition(change_edition)
+
+        if type == "arm"
+          add_mapping_changes(collection, change_node, options)
+        end
+      end
+
+      def add_mapping_changes(collection, change_node, options)
+        change_edition = collection
+                         .fetch_or_initialize(options[:schema_name], "arm")
+                         .change_editions
+                         .fetch_or_initialize(options[:version])
+
+        change_edition.mapping = extract_mapping_changes(change_node)
+      end
+
+      def extract_mapping_changes(change_node)
+        mappings = []
+
+        change_node.xpath("mapping.changes").each do |changes|
+          changes.xpath("mapping.change").each do |change|
+            mappings << { "change" => change.text }
+          end
+
+          changes.xpath("description").each do |change|
+            mappings << { "description" => change.text }
+          end
+        end
+
+        mappings
+      end
+
       def extract_change_edition(schema_changes, options)
         type = options[:type] || "schema"
-        addition_nodes = schema_changes.xpath("#{type}.additions")
-        modification_nodes = schema_changes.xpath("#{type}.modifications")
-        deletion_nodes = schema_changes.xpath("#{type}.deletions")
+        addition_nodes = schema_changes&.xpath("#{type}.additions") || []
+        modification_nodes = schema_changes&.xpath("#{type}.modifications") || []
+        deletion_nodes = schema_changes&.xpath("#{type}.deletions") || []
 
         {
-          type: type,
           version: options[:version],
           description: options[:description],
           additions: extract_modified_objects(addition_nodes),
@@ -116,18 +152,29 @@ module Stepmod
             {
               type: object.attr("type"),
               name: object.attr("name"),
+              description: converted_description(object.at("description")),
               interfaced_items: object.attr("interfaced.items"),
             }.compact
           end
         end.flatten
       end
 
+      def converted_description(description)
+        return if description.to_s.empty?
+
+        Stepmod::Utils::SmrlDescriptionConverter.convert(description)
+      end
+
       def all_resource_changes_files
-        Dir.glob(File.join(stepmod_dir, "data/resource_docs/*/resource.xml"))
+        Dir.glob(
+          File.join(stepmod_dir, "data", "resource_docs", "*", "resource.xml"),
+        )
       end
 
       def all_modules_changes_files
-        Dir.glob(File.join(stepmod_dir, "data/modules/*/module.xml"))
+        Dir.glob(
+          File.join(stepmod_dir, "data", "modules", "*", "module.xml"),
+        )
       end
 
       # rubocop:disable Layout/LineLength
