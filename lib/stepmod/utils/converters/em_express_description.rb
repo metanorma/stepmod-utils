be80a29773a96efb5dd9137ce710f46e9b23332b
HassanAkbar <hassanakbar2@gmail.com>
2023-11-14 19:14:20 +0500

    fixing issue with eqations inside italics

--- lib/stepmod/utils/converters/em_express_description.rb
+++ lib/stepmod/utils/converters/em_express_description.rb
@@ -5,14 +5,41 @@ module Stepmod
     module Converters
       class Em < ReverseAdoc::Converters::Base
         def convert(node, state = {})
-          content = treat_children(node, state.merge(already_italic: true))
-          if state[:equation]
+          italic_converted(node, state)
+        end
+
+        private
+
+        def italic_converted(node, state)
+          cloned_node = node.clone
+          equations = extract_equations(cloned_node)
+          content = treat_children(cloned_node, state.merge(already_italic: true))
+          equation_content = equations.map do |equation|
+            treat(equation, state.merge(equation: true, already_italic: true))
+          end
+
+          content = if state[:equation] && !content.strip.empty?
             "ii(#{content.strip})"
           elsif content.strip.empty? || state[:already_italic]
             content
           else
             "#{content[/^\s*/]}_#{content.strip}_#{content[/\s*$/]}"
           end
+
+          [content, equation_content].compact.join("")
+        end
+
+        def extract_equations(node)
+          equations = []
+
+          node.children.each do |n|
+            next if n.name != "eqn"
+
+            equations << n
+            n.unlink
+          end
+
+          equations
         end
       end
 
